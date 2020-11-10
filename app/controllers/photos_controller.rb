0beef1988d500d088372a1b0625143815db64b62
Chanhee <chan420420@gmail.com>
2020-11-10 14:22:38 +0900

    delete photos using jquery & ajax

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,21 +1,13 @@
 class PhotosController < ApplicationController
 
-  def create
-    @room = Room.find(params[:room_id])
-
-    if params[:images]
-      params[:images].each do |img|
-        @room.photos.create(image: img)
-      end
-
-      @photos = @room.photos
-      redirect_back(fallback_location: request.referer, notice: "Saved...")
-    end
-
-  end
-
   def destroy
+    @photo = Photo.find(params[:id])
+    book = @photo.book
+
+    @photo.destroy
+    @photos = Photo.where(book_id: book.id)
+
+    respond_to :js
 
   end
-
 end

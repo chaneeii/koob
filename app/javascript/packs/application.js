6386fec2ff8b4cb92f401b202cacc0d8f361d83a
Chanhee <chan420420@gmail.com>
2020-11-16 00:44:24 +0900

    Review View (star-rating ) : got some errors (so skip it rn)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,13 +10,15 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 require("jquery")
-require("jquery-ui")
+
 
 toastr = require("toastr")
 import "bootstrap"
 
 
 //= require jquery-ui/widgets/datepicker
+//=require packs/jquery.raty.js
+// require("packs/jquery.raty")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
